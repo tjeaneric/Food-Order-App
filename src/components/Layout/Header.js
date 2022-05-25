76b860e6aa0acde852d978ad2b56752f1111bd18
tjeaneric <ericjohn415@gmail.com>
2022-05-25 11:32:30 +0200

    first commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,15 @@
+import { Fragment } from 'react';
+
+const Header = (props) => {
+  <Fragment>
+    <header>
+      <h1>React Meals</h1>
+      <button>Cart</button>
+    </header>
+    <div>
+      <img />
+    </div>
+  </Fragment>;
+};
+
+export default Header;
